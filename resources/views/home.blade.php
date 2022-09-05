619eaacab0af43e15005cfaef8670457c92576c7
Francisco Jesus <francisco.adan@qastusoft.com>
2022-09-05 14:44:21 +0200

    Iniciado proyecto con AdminLTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'Inicio')
+
+@section('content_header')
+    <h1>Tablero</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam earum itaque aliquid quod nihil sequi soluta, cupiditate, sunt debitis cumque quo totam quis tempora quae fugit dolorum officia ab similique.</p>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
